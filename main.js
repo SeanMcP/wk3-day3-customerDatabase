d819817f7ec8145a32447c93ac49070ebd9a0fb3
Sean McPherson <seanmcp25@gmail.com>
2017-08-02 14:28:26 -0400

    Successfully pulled information from database; displayed correctly on screen; some design tweaks

--- main.js
+++ main.js
@@ -1,21 +1,56 @@
-let siteMain = document.getElementById("siteMain");
+let siteMain = document.createElement("main");
+siteMain.id = "siteMain";
 
-let template = "";
+let pageHead = document.createElement("h1");
+pageHead.id = "pageHead";
+pageHead.setAttribute("class", "upper");
+pageHead.textContent = "Internal Company Directory";
 
-  template += `<section class="bio">`
+siteMain.appendChild(pageHead);
 
-  template += `<img src=${customers.results[0].picture.large} class="headshot" />`
+document.body.appendChild(siteMain);
 
-  template += `<h2 class="cap">${customers.results[0].name.title}. ${customers.results[0].name.first} ${customers.results[0].name.last}</h2>`
+for (var i = 0; i < customers.results.length; i++) {
+  let template = "";
 
-  template += `<p class="email">${customers.results[0].email}</p>`
+    template += `<section class="bio">`
 
-  template += `<p class="address cap">${customers.results[0].location.street}
-  <br>
-  ${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}`
+    template += `<img src=${customers.results[i].picture.large} class="headshot" />`
 
-  template += `<p class="phone">${customers.results[0].phone}`
+    template += `<h2 class="upper">${customers.results[i].name.first} ${customers.results[i].name.last}</h2>`
 
-  template += `</section>`
+    template += `<p class="email upper">${customers.results[i].email}</p>`
 
-siteMain.innerHTML += template;
+    template += `<p class="address cap">${customers.results[i].location.street}
+    <br>
+    ${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode}`
+
+    template += `<p class="phone">${customers.results[i].phone}`
+
+    template += `<p class="blur">${customers.results[i].id.value}</p>`
+
+    template += `</section>`
+
+  siteMain.innerHTML += template;
+}
+// let template = "";
+//
+//   template += `<section class="bio">`
+//
+//   template += `<img src=${customers.results[0].picture.large} class="headshot" />`
+//
+//   template += `<h2 class="upper">${customers.results[0].name.first} ${customers.results[0].name.last}</h2>`
+//
+//   template += `<p class="email">${customers.results[0].email}</p>`
+//
+//   template += `<p class="address cap">${customers.results[0].location.street}
+//   <br>
+//   ${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}`
+//
+//   template += `<p class="phone">${customers.results[0].phone}`
+//
+//   template += `<p class="blur">${customers.results[0].id.value}</p>`
+//
+//   template += `</section>`
+//
+// siteMain.innerHTML += template;
